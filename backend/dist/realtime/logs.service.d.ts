7742df532dfdf6f36b7279ac7606136c39745050
loreeue <loruzqui@student.42madrid.com>
2026-05-01 12:06:55 +0200

    delete folder dist

--- backend/dist/realtime/logs.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { LogsGateway } from './logs.gateway';
-export declare class LogsService {
-    private readonly gateway;
-    constructor(gateway: LogsGateway);
-    sendLog(deployId: string, log: string): void;
-    sendStatus(deployId: string, status: string): void;
-    sendStart(deployId: string): void;
-    sendEnd(deployId: string, success: boolean): void;
-}
