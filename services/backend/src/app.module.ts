357469c78d620f50a42ccf45ac43b7a541dd2a1c
DanielCasti11o <bydanny17@gmail.com>
2026-07-18 23:25:55 +0200

    add module of auth in backend app_modules

--- services/backend/src/app.module.ts
+++ services/backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RealtimeModule } from './realtime/realtime.module';
 import { DeploymentsModule } from './deployments/deployments.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     RealtimeModule,
     DeploymentsModule,
+	AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
